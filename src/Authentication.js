83d7d22489ddd3087da0950578950c066d177be0
gertie-sheshe <gertrude.nyenyeshi@andela.com>
2018-09-10 19:51:24 +0300

    add auth

--- src/Authentication.js
+++ src/Authentication.js
@@ -1,31 +1,36 @@
 import React, {Component} from 'react';
-import {Route, Link, Redirect, BrowserRouter as Router} from 'react-router-dom';
+import {Route, Link, Redirect, withRouter, BrowserRouter as Router} from 'react-router-dom';
 import Public from './components/Public';
 import Private from './components/Private';
+import AuthService from './utils/authservice';
+import Login from './components/Login';
 
-const AuthService = {
-    isAuthenticated: false,
-    authenticate(cb) {
-        this.isAuthenticated = true;
-        setTimeout(cb, 100);
-    },
-    logout(cb) {
-        this.isAuthenticated = false;
-        setTimeout(cb, 100);
-    }
-};
 
 const SecretRoute = ({component: Component, ...rest}) => (
     <Route {...rest} render={(props) => (
-        AuthService.isAuthenticated === true ? <Component {...props}/> : <Redirect to="/login"/>
+        AuthService.isAuthenticated === true ? <Component {...props}/> : <Redirect to={{
+            pathname: '/login',
+            state: {from: props.location}
+        }}/>
     )}/>
 )
+
+const AuthStatus = withRouter(({history}) => (
+        AuthService.isAuthenticated ? (
+            <p>
+                Welcome! <button onClick={ () => {
+                    AuthService.logout(() => history.push('/'))
+                }}>Sign Out</button>
+            </p>
+        ) : (<p>You are not logged in</p>)
+))
 
 class Auth extends Component {
     render() {
         return (
             <Router>
                 <div style={{width: 1000, margin: '0 auto'}}>
+                <AuthStatus />
                     <ul>
                         <li><Link to="/public">Public</Link></li>
                         <li><Link to="/private">Private</Link></li>
@@ -34,6 +39,7 @@ class Auth extends Component {
                     <hr />
 
                     <Route path='/public' component={Public} />
+                    <Route path="/login" component={Login} />
                     <SecretRoute path='/private' component={Private} />
                 </div>
             </Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { Component } from "react";
+import AuthService from '../utils/authservice';
+import {Redirect} from 'react-router-dom';
 
-const Login = () => (
-    <div>
-        Ya need to Login, ya
-    </div>
-);
+class Login extends Component {
+    state = {
+        redirectToPreviousState: false
+    };
+
+    login = () => {
+        AuthService.authenticate(() => {
+            this.setState({redirectToPreviousState: true})
+        })
+    }
+
+    render() {
+        const { from } = this.props.location.state || {from : { pathname: '/'}};
+        const {redirectToPreviousState} = this.state;
+
+        if (redirectToPreviousState) {
+            return <Redirect to={from} />
+        }
+
+        return (
+            <div>
+                <p>You must login to view the page at {from.pathname}</p>
+                <button onClick={this.login}>Log in</button>
+            </div>
+        )
+    }
+}
 
 export default Login;
